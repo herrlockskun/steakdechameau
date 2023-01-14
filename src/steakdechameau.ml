8b06e349bd2a0e4e3bbdab7bc5c8fe07085a740f
axpronnier <axel.pronnier@gmail.com>
2023-01-14 20:53:45 +0100

    spawn chamo + player

--- src/steakdechameau.ml
+++ src/steakdechameau.ml
@@ -25,12 +25,46 @@ module Runtime = struct
           | Cons _ -> false
         else false
   end
+
+  module Entity = struct
+    type t = Chamo | Player
+
+    let to_key : t -> string = function
+      | Chamo -> "Chamo"
+      | Player -> "Player"
+
+    let get_by_guid key =
+      Orx.Config.(get get_guid) ~section ~key
+      |> Orx.Object.of_guid |> Option.get
+
+    let get what = get_by_guid (to_key what)
+
+    let get_speed what =
+      match what with
+      | Chamo -> Orx.Config.(get get_float) ~section:"Chamo" ~key:"Speed"
+      | Player -> Orx.Config.(get get_float) ~section:"Player" ~key:"Speed"
+  end
 end
 
+
+
+module Input = struct
+  let check_player () =
+    let _ = Orx.Log.log "%d" (3) in
+    let player = Runtime.Entity.get Player in
+    let _ = Orx.Log.log "%d" (4) in
+    let player_speed = Runtime.Entity.get_speed Player in
+    let right_speed = Orx.Vector.make ~x:player_speed ~y:0.0 ~z:0.0 in
+    let left_speed = Orx.Vector.mulf right_speed ~-.1.0 in
+    if Orx.Input.is_active "Left" then Orx.Object.set_speed player left_speed;
+    if Orx.Input.is_active "Right" then Orx.Object.set_speed player right_speed;
+  
+end
 
 let init () =
   let _viewport = Orx.Viewport.create_from_config_exn "Viewport" in
   (*let _chamo = Orx.Object.create_from_config_exn "ChamoObject" in*)
+  let _player = Orx.Object.create_from_config_exn "PlayerObject" in
   let chamo_spawner = Orx.Object.create_from_config_exn "ChamoSpawner" in
   Runtime.Spawner.set chamo_spawner;
   Ok ()
@@ -38,12 +72,10 @@ let init () =
 let run () =
   if Orx.Input.is_active "Quit" then
     Orx.Status.error
-  else(if Orx.Input.is_active "Left" then
-    let _ = Orx.Log.log "PRESSED" in
-    Orx.Status.ok
   else (
-    Orx.Status.ok
-  ))
+    (*let _ = Input.check_player () in*)
+    Orx.Status.ok;
+  )
 
 let () =
   (* Start the main game engine loop *)
